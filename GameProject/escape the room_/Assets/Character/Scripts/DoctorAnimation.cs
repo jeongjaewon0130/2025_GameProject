5c902c6f5f80f9dd0afbc684ae5f77b80634ad4f
kh03525 <m2256866@gmail.com>
2025-05-21 23:15:42 +0900

    [Feat] Item 추가 (Book, Card, Watch)
    [Book] Basemap, Maskmap, Normalmap 추가와 매쉬 임포트 완료
    [Card] Red,Blue,Yellow Basmap, Maskmap, Normalmap 추가와 매쉬 임포트 완료
    [Watch] Basemap, Maskmap, Normalmap 추가와 매쉬 임포트 완료
    [Prefaps] 머테리얼 적용한 매쉬들 5개 추가

--- GameProject/escape the room_/Assets/Character/Scripts/DoctorAnimation.cs
+++ GameProject/escape the room_/Assets/Character/Scripts/DoctorAnimation.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-public class Doctor : MonoBehaviour
+public class DoctorAnimation: MonoBehaviour
 {
     public Animator animator;             // Animator 연결
     public float walkDuration = 4f;       // Walk 애니메이션 2번 길이 (예: 2초짜리 Walk * 2 = 4초)
