9c68ad0610c804e945b327262ea12d876b4fb743
kh03525 <m2256866@gmail.com>
2025-06-10 10:02:57 +0900

    [Feat] Scene 1-3 배경 수정 및 조명 추가, 인벤토리 컷씬에서 안보이도록 수정

--- GameProject/escape the room_/Assets/Inventory/InventoryManager.cs
+++ GameProject/escape the room_/Assets/Inventory/InventoryManager.cs
@@ -1,24 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class InventoryManager : MonoBehaviour
 {
     public GameObject inventoryCanvasPrefab;
     private static bool isCreated = false;
+    private GameObject canvasInstance;
+
+    // 컷씬 씬 이름들 (여기에 컷씬 씬 이름을 추가하세요)
+    private HashSet<string> cutsceneSceneNames = new HashSet<string> {"CutScene"};
 
     void Awake()
     {
         if (!isCreated)
         {
             isCreated = true;
-            GameObject canvas = Instantiate(inventoryCanvasPrefab);
-            DontDestroyOnLoad(canvas);
+            canvasInstance = Instantiate(inventoryCanvasPrefab);
+            DontDestroyOnLoad(canvasInstance);
             DontDestroyOnLoad(this.gameObject);
+
+            SceneManager.sceneLoaded += OnSceneLoaded; // 씬 로드 이벤트 등록
         }
         else
         {
             Destroy(this.gameObject); // 중복 제거
         }
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (canvasInstance != null)
+        {
+            if (cutsceneSceneNames.Contains(scene.name))
+            {
+                canvasInstance.SetActive(false); // 컷씬이면 비활성화
+            }
+            else
+            {
+                canvasInstance.SetActive(true);  // 아니면 다시 활성화
+            }
+        }
+    }
+
+    void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded; // 이벤트 해제
     }
 }
