eefe74e3ceca3b0ab67ab61d6b4bdb7ef0d7aa14
minkkjean <mkkjin@g.shingu.ac.kr>
2025-06-19 00:35:24 +0900

    [Feat] 환경설정 이미지 추가

--- GameProject/escape the room_/Assets/scripts/MenuController.cs
+++ GameProject/escape the room_/Assets/scripts/MenuController.cs
@@ -12,100 +12,53 @@ public class MenuController : MonoBehaviour
 
     [Header("Settings Controls")]
     public Slider volumeSlider;
-    public TMP_Dropdown resolutionDropdown;
-    public Slider sensitivitySlider;
 
-    private Resolution[] resolutions;
-
-    
-
+    private bool isPaused = false;
 
     void Start()
     {
         Screen.fullScreen = false;
-
-        // 설정 패널 비활성화
         settingsPanel.SetActive(false);
-
-        // 해상도 목록 불러오기
-        resolutions = Screen.resolutions;
-        resolutionDropdown.ClearOptions();
-
-        int currentIndex = 0;
-        for (int i = 0; i < resolutions.Length; i++)
-        {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
-            resolutionDropdown.options.Add(new TMP_Dropdown.OptionData(option));
-
-            if (resolutions[i].width == Screen.currentResolution.width &&
-                resolutions[i].height == Screen.currentResolution.height)
-            {
-                currentIndex = i;
-            }
-        }
-
-        // 🔄 저장된 설정 불러오기
-        int savedIndex = PlayerPrefs.GetInt("ResolutionIndex", currentIndex);
-        resolutionDropdown.value = savedIndex;
-        resolutionDropdown.RefreshShownValue();
-
-        Resolution res = resolutions[savedIndex];
-        Screen.SetResolution(res.width, res.height, Screen.fullScreen);
 
         float savedVolume = PlayerPrefs.GetFloat("volume", 1f);
         volumeSlider.value = savedVolume;
-        SetVolume(savedVolume); // 볼륨 초기 적용
-
-        float savedSensitivity = PlayerPrefs.GetFloat("sensitivity", 1f);
-        sensitivitySlider.value = savedSensitivity;
+        SetVolume(savedVolume);
     }
 
-    public void StartNewGame()
+    void Update()
     {
-        Debug.Log("새 게임 시작!");
-        // 씬 전환 가능
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                ResumeGame();
+            else
+                PauseGame();
+        }
     }
 
-    public void LoadGame()
-    {
-        Debug.Log("불러오기 클릭됨");
-        // 저장된 게임 데이터 로드
-    }
-
-    public void OpenSettings()
+    public void PauseGame()
     {
         settingsPanel.SetActive(true);
+        Time.timeScale = 0f; // 게임 일시정지
+        isPaused = true;
     }
 
-    public void CloseSettings()
+    public void ResumeGame()
     {
-        // 🔉 볼륨 저장 및 적용
         float volume = volumeSlider.value;
         SetVolume(volume);
-
-        // 🎮 설정 저장
         PlayerPrefs.SetFloat("volume", volume);
-        PlayerPrefs.SetFloat("sensitivity", sensitivitySlider.value);
-        PlayerPrefs.SetInt("ResolutionIndex", resolutionDropdown.value);
         PlayerPrefs.Save();
 
         settingsPanel.SetActive(false);
+        Time.timeScale = 1f; // 게임 재개
+        isPaused = false;
     }
 
     public void QuitGame()
     {
         Debug.Log("게임 종료");
         Application.Quit();
-    }
-
-    public void ChangeResolution(int index)
-    {
-        Resolution res = resolutions[index];
-        Debug.Log($"해상도 변경: {res.width} x {res.height}");
-        Screen.SetResolution(res.width, res.height, Screen.fullScreen);
-
-        PlayerPrefs.SetInt("ResolutionIndex", index);
-        PlayerPrefs.Save();
     }
 
     public void OnVolumeChanged(float value)
@@ -118,4 +71,9 @@ public class MenuController : MonoBehaviour
         if (value <= 0.0001f) value = 0.0001f;
         audioMixer.SetFloat("MasterVolume", Mathf.Log10(value) * 20);
     }
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
 }
