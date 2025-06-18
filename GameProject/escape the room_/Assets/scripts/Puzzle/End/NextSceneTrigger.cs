453be7710f5f37b9912dde0ad13ee8dceb390076
saw51234 <jaewon9732@gmail.com>
2025-06-18 23:07:35 +0900

    [Feat] 수정본

---
+++ GameProject/escape the room_/Assets/scripts/Puzzle/End/NextSceneTrigger.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextSceneTrigger : MonoBehaviour
+{
+    public string nextSceneName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(nextSceneName);
+        }
+    }
+}
